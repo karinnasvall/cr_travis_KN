4306a60ea4ded50e02f659f7048cb13a3214b345
Eva Fernandez <evafcaceres@gmail.com>
2020-05-20 10:35:55 +0200

    fixed indentation issue

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ def subtract(a, b):
     return a - b  # <--- fix this in step 8
 
 
- uncomment the following test in step 5
+# uncomment the following test in step 5
 def test_subtract():
     assert subtract(2, 3) == -1
 
